3175d494eaad94e8b67ea541d019bc180d4c725b
thautwarm <twshere@outlook.com>
2020-10-06 03:56:22 +0900

    cleanup

--- src/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org
-module DynJIT.Main
-
-open System
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello Morlp from F#!"
-    0 // return an integer exit code
